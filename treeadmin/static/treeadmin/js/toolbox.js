12fb166b10912d4b9d48177d7912b6b362727807
Scott Sharkey <ssharkey@lanshark.com>
2014-11-30 15:03:39 -0500

    incorporate clincher's fix for jquery wrappers from pull request

--- treeadmin/static/treeadmin/js/toolbox.js
+++ treeadmin/static/treeadmin/js/toolbox.js
@@ -13,65 +13,65 @@ function extract_item_id(elem_id) {
     return 0;
 }
 
-/* Given an html snippet (in text form), parses it to extract the id attribute,
-   then replace the corresponding element in the page with the snippet. The
-   first parameter is ignored (so the signature matches what $.each expects).
-
-       replace_element(0, '<div id="replace_me">New Stuff!</div>')
- */
-function replace_element(i, html) {
-    var r_id = $(html).attr('id');
-    $('#' + r_id).replaceWith(html);
-}
-
-/* Same as above, but processes an array of html snippets */
-function replace_elements(data) {
-    $.each(data, replace_element);
-}
-
 /* Setup django csrf-token for ajax calls, necessary since Django 1.2.5 */
 /* See http://docs.djangoproject.com/en/1.2/releases/1.2.5/#csrf-exception-for-ajax-requests */
+treeadmin.jQquery(function($){
 
-treeadmin.jQuery.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        function getCookie(name) {
-            var cookieValue = null;
-            if (document.cookie && document.cookie !== '') {
-                var cookies = document.cookie.split(';');
-                for (var i = 0; i < cookies.length; i++) {
-                    var cookie = treeadmin.jQuery.trim(cookies[i]);
-                    // Does this cookie string begin with the name we want?
-                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                        break;
+    /* Given an html snippet (in text form), parses it to extract the id attribute,
+     then replace the corresponding element in the page with the snippet. The
+     first parameter is ignored (so the signature matches what $.each expects.
+     replace_element(0, '<div id="replace_me">New Stuff!</div>')
+     */
+    function replace_element(i, html) {
+        var r_id = $(html).attr('id');
+        $('#' + r_id).replaceWith(html);
+    }
+
+    /* Same as above, but processes an array of html snippets */
+    function replace_elements(data) {
+        $.each(data, replace_element);
+    }
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            function getCookie(name) {
+                var cookieValue = null;
+                if (document.cookie && document.cookie !== '') {
+                    var cookies = document.cookie.split(';');
+                    for (var i = 0; i < cookies.length; i++) {
+                        var cookie = $.trim(cookies[i]);
+                        // Does this cookie string begin with the name we want?
+                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                            break;
+                        }
                     }
                 }
+                return cookieValue;
             }
-            return cookieValue;
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                // Only send the token to relative URLs i.e. locally.
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
         }
-        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-            // Only send the token to relative URLs i.e. locally.
-            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
-    }
-});
-
-/* OnClick handler to toggle a boolean field via AJAX */
-function inplace_toggle_boolean(item_id, attr) {
-    $.ajax({
-      url: ".",
-      type: "POST",
-      dataType: "json",
-      data: { '__cmd': 'toggle_boolean', 'item_id': item_id, 'attr': attr },
-
-      success: replace_elements,
-
-      error: function(xhr, status, err) {
-          alert("Unable to toggle " + attr + ": " + xhr.responseText);
-      }
     });
 
-    return false;
-}
+    /* OnClick handler to toggle a boolean field via AJAX */
+    function inplace_toggle_boolean(item_id, attr) {
+        $.ajax({
+          url: ".",
+          type: "POST",
+          dataType: "json",
+          data: { '__cmd': 'toggle_boolean', 'item_id': item_id, 'attr': attr },
 
+          success: replace_elements,
 
+          error: function(xhr, status, err) {
+              alert("Unable to toggle " + attr + ": " + xhr.responseText);
+          }
+        });
+
+        return false;
+    }
+
+});
