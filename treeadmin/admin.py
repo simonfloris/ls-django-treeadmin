0fcaf1dd48f23c05944faa6aac6f9218cf868fd6
Emmanuelle Delescolle <emma@levit.be>
2015-10-02 16:17:50 +0200

    :bug: use  instead of has_key (which is not supported in python 3)

--- treeadmin/admin.py
+++ treeadmin/admin.py
@@ -56,7 +56,7 @@ def _build_tree_structure(cls):
         all_nodes[p_id] = []
 
         if parent_id:
-            if not all_nodes.has_key(parent_id):
+            if parent_id not in all_nodes:
                 # This happens very rarely, but protect against parents that
                 # we have yet to iterate over.
                 all_nodes[parent_id] = []
